a414d5f6bf7c531fcb68ec1d9a6d54eada0d5207
Daniel Kopecky <daniel.kopecky@ogsoft.cz>
2022-04-02 14:47:09 +0200

    laravel projekt

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 Auth::routes();
 
-Route::get('/', function () {return view("database");})->middleware('auth');
+Route::get('/', 'Controller@showDatabase')->middleware('auth');
 
 // Endpointy místností
 Route::get('/rooms', 'RoomController@getRooms')->middleware('auth');
